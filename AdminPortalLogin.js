ccc42d613887c27ac58a643fdc962f8a46e16590
Seth Guimont <ur4634zc@metrostate.edu>
2019-06-17 17:59:37 -0500

    Test Commit

---
+++ AdminPortalLogin.js
@@ -0,0 +1,12 @@
+var connection = require("./MongoConnection.js");
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
