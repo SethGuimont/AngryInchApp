7e13f64e453a7aff1426fd50e95d436eab27908d
Seth Guimont <ur4634zc@metrostate.edu>
2019-07-30 14:21:59 -0500

    copied and pasted code for the to AdminPortalLogin.js to see if we can get that working.

--- controllers/AdminPortalLogin.js
+++ controllers/AdminPortalLogin.js
@@ -2,7 +2,7 @@
 // Seth Guimont
 // This code below is to simply test referencing the javascript in the html file
 // This will be commented out as we add returning Users from Mongodb
-var attempt = 100; // Variable to count number of attempts.
+/*var attempt = 100; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate(){
     var username = document.getElementById("username").value;
@@ -57,3 +57,54 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;*/
+
+// adding this code to play with later
+
+var mongoose = require("mongoose");
+var passport = require("passport");
+var User = require("../models/User");
+
+var userController = {};
+
+// Restrict access to root page
+userController.home = function(req, res) {
+    res.render('index', { user : req.user });
+};
+
+// Go to registration page
+userController.register = function(req, res) {
+    res.render('register');
+};
+
+// Post registration
+userController.doRegister = function(req, res) {
+    User.register(new User({ username : req.body.username, name: req.body.name }), req.body.password, function(err, user) {
+        if (err) {
+            return res.render('register', { user : user });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+};
+
+// Go to login page
+userController.login = function(req, res) {
+    res.render('login');
+};
+
+// Post login
+userController.doLogin = function(req, res) {
+    passport.authenticate('local')(req, res, function () {
+        res.redirect('/');
+    });
+};
+
+// logout
+userController.logout = function(req, res) {
+    req.logout();
+    res.redirect('/');
+};
+
+module.exports = userController;
